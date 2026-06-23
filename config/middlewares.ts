69895e1d3e4c469ecf88e85abd4e02f9d731f0c2
Kioko Kennedy <kmkioko@gmail.com>
2026-06-23 23:05:40 +0300

    CORS update

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,10 +1,40 @@
+// import type { Core } from '@strapi/strapi';
+
+// const config: Core.Config.Middlewares = [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+
+// export default config;
+
+
 import type { Core } from '@strapi/strapi';
 
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'https://sports-arena-frontend-alpha.vercel.app/',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
